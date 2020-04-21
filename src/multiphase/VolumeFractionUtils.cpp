db4bbbf62b9344b681f0caf31aab22304f0945a0
gdeskos <gdeskosv@gmail.com>
2020-04-20 19:18:10 -0600

    added the VolumeFractionUtils

---
+++ src/multiphase/VolumeFractionUtils.cpp
@@ -0,0 +1,14 @@
+//
+//  VolumeFractionUtils.cpp
+//  incflo
+//
+
+#include "VolumeFractionUtils.H"
+
+using namespace amrex;
+
+void VolumeFractionUtils::reconstruct()
+{
+    BL_PROFILE("VolumeFractionUtils::reconstruct");
+}
+
