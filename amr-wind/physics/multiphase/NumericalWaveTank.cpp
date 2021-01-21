9f2462b8232e04b065b2a5e21faae7b117fc2051
gdeskos <gdeskosv@gmail.com>
2021-01-21 12:58:19 -0700

    added NWT physics sub-class

---
+++ amr-wind/physics/multiphase/NumericalWaveTank.cpp
@@ -0,0 +1,57 @@
+#include "amr-wind/physics/multiphase/NumericalWaveTank.H"
+#include "amr-wind/CFDSim.H"
+#include "AMReX_ParmParse.H"
+#include "amr-wind/fvm/gradient.H"
+#include "amr-wind/core/field_ops.H"
+
+namespace amr_wind {
+
+NumericalWaveTank::NumericalWaveTank(CFDSim& sim)
+    : m_velocity(sim.repo().get_field("velocity"))
+    , m_levelset(sim.repo().get_field("levelset"))
+{
+    amrex::ParmParse pp(identifier());
+    pp.query("amplitude", m_amplitude);
+    pp.query("wavelength", m_wavelength);
+    pp.query("water_level", m_waterlevel);
+}
+
+/** Initialize the velocity and levelset fields at the beginning of the
+ *  simulation.
+ *
+ */
+void NumericalWaveTank::initialize_fields(
+    int level, const amrex::Geometry& geom)
+{
+    auto& velocity = m_velocity(level);
+    velocity.setVal(0.0, 0, AMREX_SPACEDIM);
+
+    auto& levelset = m_levelset(level);
+    const auto& dx = geom.CellSizeArray();
+    const auto& problo = geom.ProbLoArray();
+    const amrex::Real Amp = m_amplitude;
+    const amrex::Real waveL = m_wavelength;
+    const amrex::Real water_level = m_waterlevel;
+
+    for (amrex::MFIter mfi(levelset); mfi.isValid(); ++mfi) {
+        const auto& vbx = mfi.validbox();
+        auto phi = levelset.array(mfi);
+        auto vel = velocity.array(mfi);
+
+        amrex::ParallelFor(
+            vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
+                const amrex::Real z = problo[2] + (k + 0.5) * dx[2];
+                const amrex::Real eta =
+                    water_level + Amp * std::sin(2.0 * M_PI * x / waveL);
+                phi(i, j, k) = eta - z;
+                if (z < eta) {
+                    vel(i, j, k, 0) = 0.0;
+                } else {
+                    vel(i, j, k, 0) = z;
+                }
+            });
+    }
+}
+
+} // namespace amr_wind
